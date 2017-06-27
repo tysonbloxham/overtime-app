6979c9799dc17a8b1d4598f88b5fd0781ab50106
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-06-27 16:03:17 -0600

    Used resource generator for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627194949) do
+ActiveRecord::Schema.define(version: 20170627214205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.date     "date"
+    t.text     "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
