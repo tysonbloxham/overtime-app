7419dd22f1b07cef620a26df3b9d32fca19ccc47
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-07 13:45:46 -0600

    Removed registration functionality from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:registrations]
+
   root to: 'static#homepage'
 end
