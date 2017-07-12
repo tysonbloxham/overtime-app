a5d95c64af8e09584a96427b865c70c1522d8c51
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-12 11:51:49 -0600

    Implemented edit functionality for the status

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -14,6 +14,7 @@ class PostDashboard < Administrate::BaseDashboard
     rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::Text.with_options(searchable: false),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -23,6 +24,7 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
+    :status,
     :date,
     :rationale,
   ].freeze
@@ -31,6 +33,7 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
+    :status,
     :date,
     :rationale,
     :created_at,
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 user created"
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Han", last_name: "Solo")
+
+puts "1 Admin User created"
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
